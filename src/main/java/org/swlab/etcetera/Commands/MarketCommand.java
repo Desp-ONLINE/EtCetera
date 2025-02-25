0a6893c95c8fb6c5d26ba631ab302c30423067b1
khw3268480 <khw3268480@naver.com>
2025-02-25 17:31:42 +0900

    시장 던전에서 입력하면 로비로 이동

--- src/main/java/org/swlab/etcetera/Commands/MarketCommand.java
+++ src/main/java/org/swlab/etcetera/Commands/MarketCommand.java
@@ -5,6 +5,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.swlab.etcetera.EtCetera;
 import org.swlab.etcetera.Util.CommandUtil;
 
 import java.util.List;
@@ -13,6 +14,9 @@ public class MarketCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
         Player player = (Player) commandSender;
+        if(!EtCetera.getChannelType().equals("lobby")){
+            CommandUtil.runCommandAsOP(player, "채널 워프 lobby 시장");
+        }
         if(strings.length == 0){
             CommandUtil.runCommandAsOP(player,"ah");
             return false;
