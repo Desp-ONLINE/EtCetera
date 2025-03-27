86f5e696e5afd37610a5d6be25f1b4149a2cacdf
khw3268480 <khw3268480@naver.com>
2025-03-28 00:56:28 +0900

    시세 추가

--- src/main/java/org/swlab/etcetera/Commands/MarketCommand.java
+++ src/main/java/org/swlab/etcetera/Commands/MarketCommand.java
@@ -1,16 +1,26 @@
 package org.swlab.etcetera.Commands;
 
+import com.comphenix.protocol.PacketType;
+import io.lumine.mythic.lib.math3.analysis.function.Min;
+import net.Indyuce.mmoitems.MMOItems;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.dople.transactionLog.Database.TransactionLogRepository;
 import org.jetbrains.annotations.NotNull;
 import org.swlab.etcetera.EtCetera;
 import org.swlab.etcetera.Util.CommandUtil;
 
+import javax.swing.text.NumberFormatter;
+import java.text.NumberFormat;
 import java.util.List;
 
-public class MarketCommand implements CommandExecutor{
+public class MarketCommand implements CommandExecutor {
+
+    private long SELL_PERCENTAGE = 20;
+
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
         Player player = (Player) commandSender;
@@ -38,29 +48,51 @@ public class MarketCommand implements CommandExecutor{
                     player.sendMessage("§c 가격을 입력하세요. §7§o(/시장 판매 <가격> <개수> : 개수는 입력하지 않으면 손에 든 아이템의 전부를 판매합니다.) ");
                     return false;
                 }
-                if(strings.length == 3){
-                    try{
+                if (strings.length == 3) {
+                    try {
+                        long price = Long.parseLong(strings[1]);
                         double v = Double.parseDouble(strings[2]);
                         Integer i = Integer.valueOf((int) v);
-                        CommandUtil.runCommandAsOP(player, "ah sell " + strings[1] +" "+i);
-                        System.out.println("시장판매됨");
-                        System.out.println("시장판매됨");
-                        System.out.println("시장판매됨");
-                        System.out.println("시장판매됨");
-                        System.out.println("시장판매됨");
-                        System.out.println("시장판매됨");
-                        System.out.println("시장판매됨");
+                        if (!checkPriceInRange(price, player)) {
+                            return false;
+                        }
+                        CommandUtil.runCommandAsOP(player, "ah sell " + price + " " + i);
                     } catch (NumberFormatException e) {
                         player.sendMessage("§c/시장 판매 <금액> <개수>: 개수 입력이 잘못되었습니다.");
                     }
                     return false;
                 }
-                CommandUtil.runCommandAsOP(player, "ah sell " + strings[1]);
+                long price = Long.parseLong(strings[1]);
+                if (!checkPriceInRange(price, player)) {
+                    return false;
+                }
+                CommandUtil.runCommandAsOP(player, "ah sell " + price);
                 return false;
             case "수령":
                 CommandUtil.runCommandAsOP(player, "ah claim");
                 return false;
         }
         return false;
+    }
+
+    public boolean checkPriceInRange(long price, Player player) {
+        ItemStack itemInMainHand = player.getInventory().getItemInMainHand();
+        String id = MMOItems.getID(itemInMainHand);
+        String type = MMOItems.getType(itemInMainHand).getId();
+        long averagePrice = TransactionLogRepository.getInstance().getAveragePrice(id, type, 5);
+        if(averagePrice == 0){
+            return true;
+        }
+        averagePrice *= itemInMainHand.getAmount();
+        long maximumPrice = averagePrice + averagePrice * SELL_PERCENTAGE / 100;
+        long minimumPrice = averagePrice - averagePrice * SELL_PERCENTAGE / 100;
+        NumberFormat formatter = NumberFormat.getInstance();
+        if (averagePrice != 0) {
+            if ((price > maximumPrice) || (price < minimumPrice)) {
+                player.sendMessage("§6[ 시장 경제 시스템 ] §e" + formatter.format(minimumPrice) + " §c~ §e" + formatter.format(maximumPrice) + " §c사이의 금액으로만 판매 할 수 있습니다. §7§o(평균 거래가의 " + SELL_PERCENTAGE + "% 로 판매가 제한되어 있습니다.)");
+                return false;
+            }
+        }
+        return true;
     }
 }
