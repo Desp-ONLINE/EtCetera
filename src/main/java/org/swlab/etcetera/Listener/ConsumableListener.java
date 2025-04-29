f52883f7d0872a3126dd88370ec9fc0e8fe556da
khw3268480 <khw3268480@naver.com>
2025-04-30 00:48:02 +0900

    아포칼립스에서 아이템 사용 불가

--- src/main/java/org/swlab/etcetera/Listener/ConsumableListener.java
+++ src/main/java/org/swlab/etcetera/Listener/ConsumableListener.java
@@ -1,14 +1,21 @@
 package org.swlab.etcetera.Listener;
 
 import net.Indyuce.mmoitems.api.event.item.ConsumableConsumedEvent;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.potion.PotionEffectType;
 import org.swlab.etcetera.EtCetera;
 
 public class ConsumableListener implements Listener{
 
     @EventHandler
     public void onItemConsume(ConsumableConsumedEvent e){
+        if(e.getPlayer().hasPotionEffect(PotionEffectType.SATURATION)){
+            e.getPlayer().sendMessage("§c 현재 해당 아이템을 사용할 수 없습니다.");
+            e.setCancelled(true);
+            return;
+        }
         if(e.getMMOItem().getId().equals("기타_루비")){
             if(EtCetera.getChannelType().equals("dungeon")){
                 e.setCancelled(true);
