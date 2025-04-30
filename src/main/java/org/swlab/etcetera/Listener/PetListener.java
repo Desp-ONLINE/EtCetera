54b517cb44c5ae580f47320a78840a00ed96ec14
khw3268480 <khw3268480@naver.com>
2025-04-30 17:34:40 +0900

    펫 리스너 추가

--- src/main/java/org/swlab/etcetera/Listener/PetListener.java
+++ src/main/java/org/swlab/etcetera/Listener/PetListener.java
@@ -1,18 +1,22 @@
 package org.swlab.etcetera.Listener;
 
+import fr.nocsy.mcpets.MCPets;
 import fr.nocsy.mcpets.api.MCPetsAPI;
+import fr.nocsy.mcpets.data.Pet;
 import io.lumine.mythic.bukkit.MythicBukkit;
 import io.lumine.mythic.bukkit.events.MythicMobDeathEvent;
 import io.lumine.mythic.core.mobs.ActiveMob;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 
 public class PetListener implements Listener {
     @EventHandler
-    public void onPetKill(MythicMobDeathEvent e){
-        LivingEntity killer = e.getKiller();
-        ActiveMob mythicMobInstance = MythicBukkit.inst().getMobManager().getMythicMobInstance(killer);
-//        MCPetsAPI.getAvailablePets()
+    public void onPetKill(PlayerInteractAtEntityEvent e){
+        if(e.getPlayer().getName().equals("dople_L")){
+            Pet activePet = MCPetsAPI.getActivePet(e.getPlayer().getUniqueId());
+            System.out.println("activePet.getMythicMobName() = " + activePet.getMythicMobName());            
+        }
     }
 }
