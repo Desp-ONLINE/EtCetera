6a0d4f44a443e1ef5438ea1cf9e7d8052e023ebc
khw3268480 <khw3268480@naver.com>
2025-05-08 21:17:57 +0900

    아포칼립스 개선

--- src/main/java/org/swlab/etcetera/Commands/ApocalypseCommand.java
+++ src/main/java/org/swlab/etcetera/Commands/ApocalypseCommand.java
@@ -15,9 +15,13 @@ public class ApocalypseCommand implements CommandExecutor {
         Player player = (Player) commandSender;
         String joinMessage = ColorManager.format("#D293FF /아포칼립스 입장 §f- 아포칼립스 던전에 입장할 수 있는 UI를 오픈합니다. ");
         String checkMessage = ColorManager.format("#D293FF /아포칼립스 확인 §f- 오늘 나의 아포칼립스 던전 입장권 획득 여부를 확인합니다. ");
+        String receiveMessage = ColorManager.format("#D293FF /아포칼립스 보상받기 §f- 현재 보상을 수령합니다. 수령 시 입장권이 소모됩니다.");
+        String rewardInfoMessage = ColorManager.format("#D293FF /아포칼립스 보상정보 §f- 현재 수령 가능한 보상의 정보를 확인하실 수 있습니다.");
         if (strings.length == 0) {
             player.sendMessage(joinMessage);
             player.sendMessage(checkMessage);
+            player.sendMessage(receiveMessage);
+            player.sendMessage(rewardInfoMessage);
             return false;
         }
         switch (strings[0]) {
@@ -31,10 +35,18 @@ public class ApocalypseCommand implements CommandExecutor {
             case "확인":
                 CommandUtil.runCommandAsOP(player, "웨이브던전 확인");
                 return true;
+            case "보상받기":
+                CommandUtil.runCommandAsOP(player, "웨이브던전 보상받기");
+                return true;
+            case "보상정보":
+                CommandUtil.runCommandAsOP(player, "웨이브던전 보상정보");
+                return true;
             default:
 
                 player.sendMessage(joinMessage);
                 player.sendMessage(checkMessage);
+                player.sendMessage(receiveMessage);
+                player.sendMessage(rewardInfoMessage);
                 return false;
         }
     }
