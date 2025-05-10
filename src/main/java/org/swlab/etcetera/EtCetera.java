18eaa0cb47c5664d9725e7a6975d3f874a90161a
khw3268480 <khw3268480@naver.com>
2025-05-10 17:49:06 +0900

    채널 명령어 간단화

--- src/main/java/org/swlab/etcetera/EtCetera.java
+++ src/main/java/org/swlab/etcetera/EtCetera.java
@@ -137,6 +137,7 @@ public final class EtCetera extends JavaPlugin {
         getCommand("쓰레기통").setExecutor(new TrashcanCommand());
         getCommand("장비").setExecutor(new AccCommand());
         getCommand("강화").setExecutor(new ReinforceCommand());
+        getCommand("채").setExecutor(new ChannelCommand());
         getCommand("쿠폰").setExecutor(new CouponCommand());
         getCommand("합성").setExecutor(new MergeCommand());
         getCommand("전리품").setExecutor(new RewardSellCommand());
