6f3f972ba13e545a2326ceca13a302b5a47469d0
khw3268480 <khw3268480@naver.com>
2025-06-17 17:48:39 +0900

    펫 명령어 분할

--- src/main/java/org/swlab/etcetera/Commands/PetCommand.java
+++ src/main/java/org/swlab/etcetera/Commands/PetCommand.java
@@ -19,7 +19,22 @@ public class PetCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
         Player player = (Player) sender;
-        CommandUtil.runCommandAsOP(player, "mcpets");
+        if(strings.length == 0){
+            player.sendMessage("§8[§6펫§8] >> §7/펫 소환 - §e귀여운 내 펫들을 소환할 수 있습니다!");
+            player.sendMessage("§8[§6펫§8] >> §7/펫 휴식 - §e소환한 펫들을 휴식시킵니다.");
+            return false;
+        }
+        switch (strings[0]){
+            case "소환":
+                CommandUtil.runCommandAsOP(player, "mcpets");
+                break;
+            case "휴식":
+                CommandUtil.runCommandAsOP(player, "mcpets revoke");
+                break;
+            default:
+                player.sendMessage("§8[§6펫§8] >> §7/펫 소환 - §e귀여운 내 펫들을 소환할 수 있습니다!");
+                player.sendMessage("§8[§6펫§8] >> §7/펫 휴식 - §e소환한 펫들을 휴식시킵니다.");
+        }
 
         return true;
     }
